3809f48764c0be33238fe93bdc73e8c0da1bc019
slowisfast <slowisfast2030@163.com>
2023-05-13 17:59:10 +0800

    less is more

--- temp.js
+++ temp.js
@@ -1,9 +1,17 @@
 var fs = require('fs');
+
+// rs是一个eventEmitter对象
+// 当文件打开后，会emit一个open事件
+// 当文件读完后，会emit一个end事件
 var rs = fs.createReadStream('./demofile.txt');
 
-// 
+// 为open事件绑定一个回调函数
 rs.on('open', function () {
   console.log('The file is open');
+});
+// 为end事件绑定一个回调函数
+rs.on('end', function () {
+    console.log('The file is end');
 });
 
 
